e18cf67a6ad1a3b54fa0b87191ee37a470493b21
YevgaKim <jekanew09@gmail.com>
2022-10-25 11:32:58 +0300

    com 2 fix eng

--- parse_word_eng.py
+++ parse_word_eng.py
@@ -14,32 +14,53 @@ for table in document.tables:
         for j, cell in enumerate(row.cells):
             if cell.text:
                 df[i][j] = cell.text
+    
 
 
 for i in df:
-    if i[-1]=="":
-        continue
+    if "\n" in i[-1]:
+        i[-1] = re.sub(r"\n","",i[-1])
+    if i[-1]=="" and not re.search(r"/\W*\S*/",i[1]) and not "adj" in i[1]:
+        continue    
     eng = i[1].split("\n")
-    eng.pop()
+    if len(eng)!=1:
+        eng.pop()
     word=""
     for j in eng:
         word+=j
-    word = word.split(",")
+    if "," in word and not re.search(r"\d\)",word):
+        word = word.split(",")
+    elif " adj" in word:
+        word = word.split("adj")
+        if "," in word[0]:
+            word[0]=re.sub(r",","",word[0])
+    else:
+        word=[word]
+
     answer=""
     if len(word)==2:
         answer=word[0].strip()
     elif len(word)>=3:
         answer=f"{word[0].strip()}, {word[1].split('=')[-1].strip()}"
     else:
-        print(i)
-        answer="ERROR"
+        if re.search(r"\d\)",word[0]):
+            with open("data\\words\\words_eng.txt","a",encoding="utf-8") as f:
+                f.write(f"{i[-1]}")
+                continue
+        answer="error"
+
     if ", n " in answer:
         answer = answer.split(", n ")[0]
     elif ", v " in answer:
         answer = answer.split(", v ")[0]
+    elif ", adj" in answer:
+        answer = answer.split(", adj")[0]
     elif "/" in answer:
         answer = re.sub(r"/\W*\S*/","",answer)
+    elif not re.search(r"\w",i[-1]) and i[-1]!="" and i[-1]!="\n":
+        i[-1]="error"
+    
     with open("data\\words\\words_eng.txt","a",encoding="utf-8") as f:
-        f.write(f"{answer.lower()} - {i[-1].strip().lower()}\n")
+        f.write(f"\n{answer.lower().strip()} - {i[-1].strip().lower()}")
     
     
