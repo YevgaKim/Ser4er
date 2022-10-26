8d126923ce9d45de1eddfc1320ceac1adbb13a50
YevgaKim <jekanew09@gmail.com>
2022-10-26 18:50:42 +0300

    com beta 1.0

--- Ser4er.py
+++ Ser4er.py
@@ -1,20 +1,61 @@
 from tkinter import *
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 
 from PIL import Image, ImageTk
+
+from sys.parse_quizlet_tur import get_tur
+from sys.parse_word_eng import get_eng
 
 screen = Tk()
 
 screen.resizable(width=False,height=False)
-screen.geometry("300x350")
+screen.geometry("300x370")
 screen.title("Ser4er")
 screen.iconbitmap("sys\\icons\\icon.ico")
 
 color_1="black"
 color_2="white"
 screen["bg"]=color_2
-file = "eng"
+file = ""
+
+def parse_tur():
+    mb.showinfo(
+        title="Turkish", 
+        message="Parsing started. Just wait")
+    try:
+        get_tur()
+    except:
+        mb.showerror(
+            "Error", 
+            "ERROR")
+
+    mb.showinfo(
+        title="Turkish", 
+        message="Parsing finished")
+
+def parse_eng():
+    file_name = fd.askopenfilename()
+    mb.showinfo(
+        title="English", 
+        message="Parsing started. Just wait")
+    try:
+        get_eng(file_name)
+    except:
+        mb.showerror(
+            "Error", 
+            "ERROR")
+
+    mb.showinfo(
+        title="English", 
+        message="Parsing finished")
+
 
 def search():
+    if var.get() == 0:
+        file="eng"
+    elif var.get() == 1:
+        file = 'tur'
     text.delete(1.0, END)
     word = ent.get().lower().strip()
     with open(f"data\\words\\words_{file}.txt","r",encoding="utf-8") as f:
@@ -33,11 +74,11 @@ ent = Entry(screen,
 ent.place(width=200,height=27,x=53,y=5)
 
 text = Text(wrap=WORD,font="20",bg=color_2,fg=color_1)
-text.place(width=275,height=250, x=10,y=80)
+text.place(width=275,height=260, x=10,y=80)
 
 
 scroll = Scrollbar(command=text.yview)
-scroll.place(width=15,height=250,x=285,y=80)
+scroll.place(width=15,height=260,x=285,y=80)
 
 text.config(yscrollcommand=scroll.set)
 
@@ -54,12 +95,25 @@ b_search.place(width = 80,height=25 ,x=110,y=40)
 var = IntVar()
 var.set(0)
 eng = Radiobutton(text="English",
-                  variable=var, value=0)
-tur = Radiobutton(text="Turkey",
-                  variable=var, value=1)
-eng.place(x=70,y=20)
-tur.place(x=240,y=20)
+                  variable=var, value=0
+                  ,bg=color_2)
 
+tur = Radiobutton(text="Turkish",
+                  variable=var, value=1
+                  ,bg=color_2)
+
+eng.place(x=30,y=40)
+tur.place(x=210,y=40)
+
+mainmenu = Menu(screen) 
+screen.config(menu=mainmenu)
+
+filemenu = Menu(mainmenu, tearoff=0)
+filemenu.add_command(label="Turkish",command=parse_tur)
+filemenu.add_command(label="English", command=parse_eng)
+ 
+mainmenu.add_cascade(label="Parse",
+                     menu=filemenu)
 
 
 
---
+++ sys/parse_quizlet_tur.py
@@ -0,0 +1,47 @@
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+def get_tur():
+    driver = webdriver.Chrome(
+            executable_path="C:\\Users\\ЯкименкоЄвгенійСергі\\source\\repos\\Ser4er\\sys\\chromedriver",
+        )
+    try:
+        
+        driver.get(url="https://quizlet.com")
+        time.sleep(80)
+
+        with open("sys\\index.html", "w",encoding="utf-8") as file:
+            file.write(driver.page_source)
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
+
+    with open("sys\\index.html","r",encoding="utf-8") as f:
+        src = f.read()
+
+    soup = BeautifulSoup(src,"lxml")
+
+    words_tur= soup.findAll("span",class_="TermText notranslate lang-tr")
+    tur=[]
+    for i in words_tur:
+        word=i.text
+        tur.append(word)
+
+    words_ukr= soup.findAll("span",class_="TermText notranslate lang-uk")
+    ukr=[]
+    for i in words_ukr:
+        word=i.text
+        ukr.append(word)
+
+    for i in range(len(tur)):
+        with open("data\\words\\words_tur.txt","a",encoding="utf-8") as f:
+            f.write(f"{tur[i].lower()} - {ukr[i].lower()}\n")
+
+if __name__=="__main__":
+    pass
